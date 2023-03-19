6a62d568adab67de0883a6821c9de98491e6e955
ozodbekernazarov6642 <128281409+ozodbekernazarov6642@users.noreply.github.com>
2023-03-19 18:23:06 +0500

    16-dars
    2-amaliyot

---
+++ 16-dars/16-dars 2-amaliyot.py
@@ -0,0 +1,29 @@
+Abu_Abduloh_Muhammad_Ibn_Ismoil={
+    'ism':"Abu Abduloh Muhammad Ibn Ismoil",
+    'asarlar':[
+        "Al-jome' as sahih",
+        "Al-adab al-mufrad",
+        "At-tarix al-kabir",
+        "At-tarix as-sag'ir"
+    ],
+}
+Abdulla_Qodiriy={
+    'ism':"AbdullaQodiriy",
+    'asarlar':[
+        "O'tka kunlar",
+        "Mehrobdan chayon",
+        "Obid ketmon"
+                 ]
+}
+Erkin_Vohidov={
+    'ism':"ErkinVohidov",
+    'asarlar':[
+        "Tong nafasi",
+        "Qo'shiqlarim sizga",
+        "O'zbegim",
+        "Qiziquvchan Matmusa"
+    ]
+}
+b_shaxslar=[Erkin_Vohidov,Abdulla_Qodiriy,Abu_Abduloh_Muhammad_Ibn_Ismoil]
+for _asarlar in b_shaxslar:
+    print(f"{_asarlar['ism']}ning mashxur asarlari:\n{_asarlar['asarlar']}")
