61988c808d4dd2b96b2a9316b3f582fe73c27f3d
ozodbekernazarov6642 <128281409+ozodbekernazarov6642@users.noreply.github.com>
2023-03-19 14:19:42 +0500

    14-dars
    2-amaliyot

---
+++ 14-dars/14-dars 2-amalilyot.py
@@ -0,0 +1,6 @@
+y_taom={'ozodbek':"Osh",
+        'Ali':"manti",
+        'Sardor':"salat",
+        'Mirjalol':"besh barmoq"
+        }
+print(f"Mirjalol akam {y_taom['Mirjalol']}ni yoqtiradi.\nSardor akam {y_taom['Sardor']}ni yoqtiradi.\nAli akam {y_taom['Ali']}ni yoqtiradi")
