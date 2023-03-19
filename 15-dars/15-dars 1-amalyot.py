6630f9215fc8b0a989b9191fd1c381afd40e3dd9
ozodbekernazarov6642 <128281409+ozodbekernazarov6642@users.noreply.github.com>
2023-03-19 15:16:50 +0500

    Create 15-dars 1-amalyot.py

---
+++ 15-dars/15-dars 1-amalyot.py
@@ -0,0 +1,11 @@
+python_a={'Intager':"Butun son",
+          'String':'Satr',
+          'Float':"Haqiqiy son",
+          'Bool':"Mantiqiy qiymat",
+          'If':"Agar",
+          'Else':'Boshqa',
+          'Del':"O'chirish",
+          'Remove':"Olib tashlash"
+          }
+for keys,values in sorted(python_a.items()):
+    print(f"{keys}-{values}")
