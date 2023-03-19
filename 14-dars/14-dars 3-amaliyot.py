e55f72bf3cf97fdf963a438b550fb4d0d6d5999c
ozodbekernazarov6642 <128281409+ozodbekernazarov6642@users.noreply.github.com>
2023-03-19 14:34:18 +0500

    14-dars
    3-amaliyot

---
+++ 14-dars/14-dars 3-amaliyot.py
@@ -0,0 +1,11 @@
+python_a={'intager':"Butun son",
+          'string':'Satr',
+          'float':"Haqiqiy son",
+          'Bool':"Mantiq",
+          'if':"Agar",
+          'else':'Boshqa',
+          'del':"O'chirish",
+          'remove':"Olib tashlash"
+          }
+k_soz=input("Kalit so'zni kiriting:")
+print(python_a.get(k_soz,"Bunday kalitso'zi yo'q"))
