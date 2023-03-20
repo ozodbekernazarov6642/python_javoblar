c4bd97f611a43ed439a8c58a167717e4ff510d46
ozodbekernazarov6642 <128281409+ozodbekernazarov6642@users.noreply.github.com>
2023-03-20 13:48:49 +0500

    17-dars
    3-amaliyot

---
+++ 17-dars/17-dars 3-amaliyot.py
@@ -0,0 +1,14 @@
+savol ="Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+savol += "Musbat son kiriting "
+savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+
+while True:
+    qiymat = input(savol)
+    if qiymat=='exit':
+        break
+    elif int(qiymat)<0:
+        continue
+    else:
+        ildiz = float(qiymat)**(0.5)
+        print(f"{qiymat} ning ildizi {ildiz} ga teng")
+print("Dastur tugadi")
