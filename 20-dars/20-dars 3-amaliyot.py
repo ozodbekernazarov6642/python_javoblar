5939fd50da34a7dc3b78c789892057a61d5bc6e1
ozodbekernazarov6642 <128281409+ozodbekernazarov6642@users.noreply.github.com>
2023-03-21 15:05:24 +0500

    20-dars
    3-amaliyot

---
+++ 20-dars/20-dars 3-amaliyot.py
@@ -0,0 +1,7 @@
+def _max(a,b,c):
+    sonlar=[a,b,c]
+    return max(sonlar)
+a=int(input("1-sonni kiriting:"))
+b=int(input("2-sonni kiriting:"))
+c=int(input("3-sonni kiriting:"))
+print(_max(a,b,c))
