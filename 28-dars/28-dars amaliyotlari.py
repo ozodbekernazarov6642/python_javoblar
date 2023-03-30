1af5112cb445e65efa011dfd7742755e64ba4c60
ozodbekernazarov6642 <128281409+ozodbekernazarov6642@users.noreply.github.com>
2023-03-30 19:53:11 +0500

    28-dars
    28-dars amaliyotlari

---
+++ 28-dars/28-dars amaliyotlari.py
@@ -0,0 +1,55 @@
+## 1-AMALIYOT
+#class User:
+#    def __init__(self,ism,foydalanuvchi_ismi,t_raqam,e_manzil):
+#        self.name=ism
+#        self.user_name=foydalanuvchi_ismi
+#        self.namber=t_raqam
+#        self.mail=e_manzil
+#user1=User('Ozodbek','_ozodbek_ernazarov',931426642,'ozodbekernazarov6642@gmail.com')
+#user2=User('Sardor',"sari_k0",933234555,'sardorernazarov4555@gmail.com')
+#user3=User('Alibek','ernazarovalibek_0_6_0_7',939101127,'ernazarovalibek0607@gmail.com')
+#user4=User('Mirjalol','mirjalol0624',941670624,'mirjalolernazarov0624@gmail.com')
+## 2-AMALIYOT
+#class User:
+#    def __init__(self,ism,foydalanuvchi_ismi,t_raqam,e_manzil):
+#        self.name=ism
+#        self.user_name=foydalanuvchi_ismi
+#        self.namber=t_raqam
+#        self.mail=e_manzil
+#    def get_info(user):
+#        chop_etish = (f"Foydalanuvchi nomi:{user.user_name}\n"
+#                      f"Ism Famiyyasi:{user.name}\n"
+#                      f"Telefon raqam:{user.namber}\n"
+#                      f"email:{user.mail}")
+#        return chop_etish
+#user1=User('Ozodbek','_ozodbek_ernazarov',931426642,'ozodbekernazarov6642@gmail.com')
+#user2=User('Sardor',"sari_k0",933234555,'sardorernazarov4555@gmail.com')
+#user3=User('Alibek','ernazarovalibek_0_6_0_7',939101127,'ernazarovalibek0607@gmail.com')
+#user4=User('Mirjalol','mirjalol0624',941670624,'mirjalolernazarov0624@gmail.com')
+#print(user1.get_info())
+## 3-AMALIYOT
+#class User:
+#    def __init__(self,ism,foydalanuvchi_ismi,t_raqam,e_manzil):
+#        self.name=ism
+#        self.user_name=foydalanuvchi_ismi
+#        self.namber=t_raqam
+#        self.mail=e_manzil
+#    def get_name(self):
+#        return self.name
+#    def get_user_name(self):
+#        return self.user_name
+#    def get_namber(self):
+#        return self.namber
+#    def get_mail(self):
+#        return self.mail
+#    def get_info(user):
+#        chop_etish = (f"Foydalanuvchi nomi:{user.get_name()}\n"
+#                      f"Ism Famiyyasi:{user.get_user_name()}\n"
+#                      f"Telefon raqam:{user.get_namber()}\n"
+#                      f"email:{user.get_mail()}")
+#        return chop_etish
+#user1=User('Ozodbek','_ozodbek_ernazarov',931426642,'ozodbekernazarov6642@gmail.com')
+#user2=User('Sardor',"sari_k0",933234555,'sardorernazarov4555@gmail.com')
+#user3=User('Alibek','ernazarovalibek_0_6_0_7',939101127,'ernazarovalibek0607@gmail.com')
+#user4=User('Mirjalol','mirjalol0624',941670624,'mirjalolernazarov0624@gmail.com')
+#print(user2.get_info())
